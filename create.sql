12be3f428ff5a6a2d0935fb776ef11608d9f21f8
Marcos Di Giovanni <marcosdgmarques@gmail.com>
2020-05-13 19:42:59 -0300

    Fix Create.sql file (#4)
    * Implement complex database Key
    
    * Improve DB Population
    
    * Fix Create.sql file

--- create.sql
+++ create.sql
@@ -1,5 +1,5 @@
 create sequence hibernate_sequence start with 1 increment by 1
-create table movimento_manual (num_lancamento integer not null, des_descricao varchar(255), dat_mes integer, dat_movimento timestamp, cod_usuario varchar(255), val_valor float, dat_ano integer, cod_produto integer, cod_cosif integer, primary key (num_lancamento))
+create table movimento_manual (dat_mes integer not null, num_lancamento integer not null, dat_ano integer not null, des_descricao varchar(255), dat_movimento timestamp, cod_usuario varchar(255), val_valor float, cod_produto integer, cod_cosif integer, primary key (dat_mes, num_lancamento, dat_ano))
 create table produto (cod_produto integer not null, des_produto varchar(255), sta_status varchar(255), primary key (cod_produto))
 create table produto_cosif (cod_cosif integer not null, cod_classificacao varchar(255), sta_status varchar(255), cod_produto integer, primary key (cod_cosif))
 alter table movimento_manual add constraint fk_movimento_manual_produto foreign key (cod_produto) references produto
